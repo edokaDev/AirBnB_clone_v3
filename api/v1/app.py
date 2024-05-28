9080b919354b418f99ce9a55bbcda3e4cb3e89e0
edoka <peter.edoka.242464@unn.edu.ng>
2024-05-28 15:22:48 +0100

    return not found with 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """API entry point."""
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -24,7 +24,7 @@ def teardown(error):
 @app.errorhandler(404)
 def not_found(error):
     """Handle 404."""
-    return {"error": "Not found"}
+    return make_response({"error": "Not found"}, 404)
 
 
 if __name__ == '__main__':
